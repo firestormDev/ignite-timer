7eecd2373126d1ba81ae44b009eee736c6fcd3ab
Luiz Tinti <79257881+firestormDev@users.noreply.github.com>
2024-05-30 18:56:44 -0300

    feat: Implementing History page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,46 @@
+import { HistoryContainer, Historylist } from './styles'
+
 export function History() {
   return (
-    <div>
-      <h1>History</h1>
-    </div>
+    <HistoryContainer>
+      <h1>Meu Historico</h1>
+
+      <Historylist>
+        <table>
+          <thead>
+            <th>Tarefa</th>
+            <th>Duração</th>
+            <th>Início</th>
+            <th>Status</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluido</td>
+            </tr>
+          </tbody>
+        </table>
+      </Historylist>
+    </HistoryContainer>
   )
 }
